a3b308e6c0c3b5e54df8e779449ebb9a8e84e3af
Marcelo Silveira <marcelingsm@gmail.com>
2019-09-13 17:00:48 -0300

    Primeiro programa

---
+++ PrimeiroPrograma/PrimeiroPrograma/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace PrimeiroPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String produto1 = "Computador";
+            String produto2 = "Mesa de escritorio";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("Produtos:");
+            Console.WriteLine(produto1
+                + ", cujo preco é $ "
+                + preco1.ToString("F2")
+                + "\n" + produto2
+                + ", cujo preco é $ "
+                + preco2.ToString("F2")
+                + "\n\n" + "Registro: "
+                + idade + " ano de idade, codigo "
+                + codigo + " e genero: "
+                + genero + "\n\n"
+                + "Medida com oito casas decimais: "
+                + medida.ToString("F8")
+                + "\nArredodado (Tres casas decimais): "
+                + medida.ToString("F3")
+                + "\nSeparador decimal invariant culture: "
+                + medida.ToString("F3", CultureInfo.InvariantCulture)
+                );
+        }
+    }
+}
