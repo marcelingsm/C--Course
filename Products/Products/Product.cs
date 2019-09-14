9aa1ef04df8d17b5836ffb6b19bc4212911766eb
Marcelo Silveira <marcelingsm@gmail.com>
2019-09-14 14:47:17 -0300

    class

---
+++ Products/Products/Product.cs
@@ -0,0 +1,39 @@
+﻿
+namespace Products
+{
+    class Product
+    {
+        public string Name;
+        public double Preco;
+        public int Amount;
+
+
+        public double totalValueStock()
+        {
+            return Preco * Amount;
+        }
+
+        public void addProduct(int Amount)
+        {
+            this.Amount += Amount;
+        }
+
+        public void removeProduct(int Amount)
+        {
+            this.Amount -= Amount;
+        }
+
+
+        public override string ToString()
+        {
+            return "Dados do produto: "
+                 + Name
+                 + ", R$ "
+                 + Preco.ToString("F2")
+                 + ", "
+                 + Amount
+                 + " Unidades, Total: R$"
+                 + totalValueStock().ToString("F2");
+        }
+    }
+}
---
+++ Products/Products/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+
+namespace Products
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+
+            Console.Write("Insira o Nome do produto: ");
+            product.Name = Console.ReadLine();
+            Console.Write("Insira o preco do produto: ");
+            product.Preco = double.Parse(Console.ReadLine());
+            Console.Write("Insira o quantidade no estoque: ");
+            product.Amount = int.Parse(Console.ReadLine());
+
+
+
+            int menu = 0;
+            int amount = 0;
+            while (menu != 4)
+            {
+
+                Console.WriteLine(
+                "(1) Inserir no estoque\n"
+                + "(2) Remover do estoque\n"
+                + "(3) Imprimi\n"
+                + "(4) Sair"
+                 );
+
+                Console.WriteLine("Insira a opicao desejada:");
+                menu = int.Parse(Console.ReadLine());
+
+                switch (menu)
+                {
+                    case 1:
+                        Console.WriteLine("quantos deseja Adicionar?");
+                        amount = int.Parse(Console.ReadLine());
+                        product.addProduct(amount);
+                        break;
+                    case 2:
+                        Console.WriteLine("quantos deseja Remover?");
+                        amount = int.Parse(Console.ReadLine());
+                        product.removeProduct(amount);
+                        break;
+                    case 3:
+                        Console.WriteLine(product);
+                        Console.ReadLine();
+                        break;
+
+                    default:
+                        break;
+                }
+                Console.Clear();
+            }
+        }
+    }
+}
