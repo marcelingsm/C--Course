870697d12256f60218468798d2ace7d43fd6d964
Marcelo Silveira <marcelingsm@gmail.com>
2019-09-14 16:23:06 -0300

    class POO

---
+++ Bank/Bank/Account.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+
+namespace Bank
+{
+    class Account
+    {
+        public int AccountNumber { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; private set; }
+
+        public Account(int AccountNumber, string Holder)
+        {
+            this.AccountNumber = AccountNumber;
+            this.Holder = Holder;
+        }
+
+        public Account(int AccountNumber, string Holder,double Balance)
+        {
+            this.AccountNumber = AccountNumber;
+            this.Holder = Holder;
+            Deposit(Balance);
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount + 5;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                +AccountNumber
+                +", Titular: "
+                +Holder
+                +", Saldo: $ "
+                +Balance.ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Bank/Bank/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = null;
+
+            Console.Write("Enter account number:");
+            int accountNumber = int.Parse(Console.ReadLine());
+
+
+            Console.Write("Enter account Holder:");
+            string name = Console.ReadLine();
+
+
+            Console.Write("Initial deposit? (y=yes/n=no)");
+            char confirm = char.Parse(Console.ReadLine());
+
+
+            if (confirm == 'y' || confirm == 'Y')
+            {
+                Console.Write("Deposit amount:");
+                double amount = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+                account = new Account(accountNumber,name,amount);
+            }
+            else if(confirm == 'n' || confirm == 'N')
+            {
+                account = new Account(accountNumber, name);
+            }
+            else { Console.WriteLine("Value is not valid!"); }
+
+            Console.WriteLine(account.ToString());
+
+            Console.Write("Enter amount deposit: ");
+            account.Deposit(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine("Updated:");
+            Console.WriteLine(account.ToString());
+
+            Console.Write("Enter amount withdraw: ");
+            account.Withdraw(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine("Updated:");
+            Console.WriteLine(account.ToString());
+        }
+    }
+}
