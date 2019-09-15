12d4d6e473e80df93f839d52c7ea79bd1b5bffcc
Marcelo Silveira <marcelingsm@gmail.com>
2019-09-15 12:25:59 -0300

    Vetores

---
+++ Alugueis/Alugueis/Estudante.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Alugueis
+{
+    class Estudante
+    {
+        public int NumeroQuarto { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+
+        public Estudante(int NumeroQuarto,string Nome, string Email)
+        {
+            this.NumeroQuarto = NumeroQuarto;
+            this.Nome = Nome;
+            this.Email = Email;
+        }
+
+        public override string ToString()
+        {
+            return "Nome: "
+                + Nome
+                + "-  Email: "
+                + Email
+                + "-  Quarto: "
+                + NumeroQuarto;
+        }
+    }
+}
---
+++ Alugueis/Alugueis/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Alugueis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] vec = new Estudante[10];
+            Console.WriteLine("Digite o valor de Estudantes que vao entrar:");
+            int n = int.Parse(Console.ReadLine());
+
+            if (n > 10)
+            {
+                Console.WriteLine("So possue 10(dez) quartos disponiveis!");
+                n = 10;
+            }
+
+            for (int i=0; i<n; i++)
+            {
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                vec[quarto-1] = new Estudante(quarto, nome, email);
+                Console.Clear();
+            }
+
+            Console.WriteLine("Quatos Alugados");
+            for (int i=0; i<10;i++)
+            {
+                if (vec[i]!=null)
+                {
+                    Console.WriteLine(vec[i]);
+                }
+            }
+        }
+    }
+}
